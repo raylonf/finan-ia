d5b449be2b5ffc8345d68285052792b48ffcab4c
raylonf <raylon.f@hotmail.com>
2026-08-24 22:50:16 -0300

    fix: logo com fundo transparente - remover mix-blend-mode

--- src/components/OwlIcon.tsx
+++ src/components/OwlIcon.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 
-export function OwlIcon({ className = 'w-5 h-5', invert = false }: { className?: string; invert?: boolean }) {
+export function OwlIcon({ className = 'w-5 h-5' }: { className?: string }) {
   return (
     <img
       src="/owl-logo.png"
       alt="Finan IA"
-      className={`${className} object-contain ${invert ? 'invert' : ''}`}
-      style={{ mixBlendMode: 'multiply' }}
+      className={`${className} object-contain`}
     />
   )
 }
